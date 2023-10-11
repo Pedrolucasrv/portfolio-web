c9ee2198abb22a239b59ed8f01a33088f1f34d95
Pedro Lucas <pedro.lucasrv@outlook.com>
2023-10-11 16:22:24 -0300

    curriculum

--- src/components/Header/Links/index.tsx
+++ src/components/Header/Links/index.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import { Button } from "../../Button"
 import {  motion } from "framer-motion"
+import { useRouter } from "next/navigation"
 
 export const Links = () =>{
+    const router = useRouter()
+
     return(
         <>
             <nav className="flex flex-col md:flex-row mt-5 gap-7 items-center">
@@ -10,7 +13,7 @@ export const Links = () =>{
                 <Link className="text-slate-300 text-sm transition-all  border-secondary hover:-translate-y-[2px]" href=''><span className="text-secondary">02.</span> Experiências</Link>
                 <Link className="text-slate-300 text-sm transition-all  border-secondary hover:-translate-y-[2px]" href=''><span className="text-secondary">03.</span> Trabalho</Link>
                 <Link className="text-slate-300 text-sm transition-all  border-secondary hover:-translate-y-[2px]" href=''><span className="text-secondary">04.</span> Contato</Link>
-                <Button className="text-sm">Currículo</Button>
+                <Button onClick={()=>{router.push('/curriculum.pdf')}} className="text-sm">Currículo</Button>
             </nav>
         </>
     )
